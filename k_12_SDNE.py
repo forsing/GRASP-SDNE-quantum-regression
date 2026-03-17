4d4fc74781c7923b84df423c337471b8752d4e8f
ForsIng <forsing@gmail.com>
2026-03-17 18:24:37 +0100

    Update CSV path and modify prediction values

--- k_12_SDNE.py
+++ k_12_SDNE.py
@@ -23,7 +23,7 @@ from qiskit_machine_learning.utils import algorithm_globals
 from qiskit.circuit.library import ZZFeatureMap, TwoLocal
 from qiskit.quantum_info import Statevector, Pauli
 
-CSV_PATH = "/Users/4c/Desktop/GHQ/data/loto7hh_4580_k21.csv"
+CSV_PATH = "/data/loto7hh_4580_k21.csv"
 
 df = pd.read_csv(CSV_PATH)
 print()
@@ -194,7 +194,7 @@ def main():
     print()
     """
     Predikcija (SDNE + kvantna regresija, deterministički, strukturno):
-    (8, 11, 23, 26, 33, 34, 35)
+    (8, 11, x, y, z, 34, 35)
     """
 
     print()
